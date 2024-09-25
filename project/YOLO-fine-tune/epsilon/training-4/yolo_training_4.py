901926de21fa9c9053fd2c071160b8b7330074a5
sooyong <exceldra5@kaist.ac.kr>
2024-09-25 16:52:44 +0900

    update requirements and gitignore

--- project/YOLO-fine-tune/epsilon/training-4/yolo_training_4.py
+++ project/YOLO-fine-tune/epsilon/training-4/yolo_training_4.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO("yolov10n.pt")
 
 model.train(data="/home/sooyong/datasets/yolo-dataset-epsilon/textbox(1,1)/data.yaml",
-            epochs=50, imgsz=640, batch=16, lr0=0.01, optimizer='Adam', save_period=10, device='0,1')
+            epochs=50, imgsz=640, batch=16, lr0=0.01, optimizer='Adam', save_period=10, device='0')
