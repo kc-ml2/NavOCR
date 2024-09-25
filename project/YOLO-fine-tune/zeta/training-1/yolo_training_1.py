4d147fcec6ec54197fb2c40e0e1f12c1183bace8
sooyong <exceldra5@kaist.ac.kr>
2024-09-25 16:13:08 +0900

    add training file for each server

---
+++ project/YOLO-fine-tune/zeta/training-1/yolo_training_1.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov10n.pt")
+
+model.train(data="/home/sooyong/datasets/yolo-dataset-zeta/textbox(1,1)/data.yaml",
+            epochs=50, imgsz=640, batch=8, lr0=0.01, optimizer='Adam', save_period=10, device='0,1')
